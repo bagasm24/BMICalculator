85cbe2cd49e1724d89728899b206d89e7e2307da
bagasm24 <mbagas368@gamil.com>
2023-10-04 05:09:36 +0700

    membuat logic pada javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+let weight = document.getElementById("weight")
+let height = document.getElementById("height")
+let btnSubmit = document.getElementById("btn-bmi")
+let contentResult = document.getElementById("content-result");
+
+btnSubmit.addEventListener("click", (event) => {
+    event.preventDefault();
+    function rumusBMI(weight, height){
+        let bmi = weight / ((height / 100)**2)
+        return bmi
+    }
+    function categoriesBMI(hasIlBMI) {
+        if(hasIlBMI < 18.5){
+            return "Underweight"
+        }else if(hasIlBMI >= 18.5 && hasIlBMI <= 24.9){
+            return "Normal"
+        }else if(hasIlBMI >= 25 && hasIlBMI <= 29.9){
+            return "Overeweight"
+        }else if(hasIlBMI >= 30){
+            return "Obesity"
+        }
+    }
+    let bmiResult = rumusBMI(weight.value, height.value);
+    let bmiCategory = categoriesBMI(bmiResult);
+    contentResult.innerHTML = `BMI Anda adalah: ${bmiResult.toFixed(2)} ${bmiCategory}`;
+})
